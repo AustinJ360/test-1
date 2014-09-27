2ce9d3e4aa2e90c0bcb79bf8a417cc97c7260c13
Austin Juntra <AustinJuntra@gmail.com>
2014-09-27 08:26:48 -0700

    test

---
+++ main.js
@@ -0,0 +1,20 @@
+
+$('#myModal').modal('toggle')
+$('#myModal').modal('show')
+$('#myModal').modal('hide')
+ $('.carousel').carousel()
+  $('.carousel').carousel({
+  interval: 2000
+ $('#myModal').modal(options)
+$('#myModal').modal({
+  keyboard: false;
+$('#myModal').modal('toggle')
+$('#myModal').modal('show')
+$('#myModal').modal('hide')
+$('#myModal').on('hidden.bs.modal', function (e) {
+  // do something...
+})
+})
+})
+})
+})
